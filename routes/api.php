01f7a5e1ec0271fc32689a8a8e19e755bed36e6f
Thura <waithura13579@gmail.com>
2022-05-26 12:39:11 +0630

    feature/waithuratun/middleware_user_admin assign for protecting user and admin routes by middleware

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\User\UserAuthController;
 use App\Http\Controllers\Course\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -23,19 +24,21 @@ Route::post('/register/confirm', [UserController::class, 'registerConfirm']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/user/logout', [UserAuthController::class, 'logoutUser']);
-    Route::get('/user/list',[UserController::class, 'index']);
+    Route::get('/course/get_all', [CourseController::class, 'getAllCourse']);
+    Route::get('/course/search/{keyword}', [CourseController::class, 'searchCourse']);
+    Route::get('/course/edit/{id}', [CourseController::class, 'editCourse']);
+});
+
+Route::middleware([IsAdmin::class])->group(function(){
+    Route::post('/course/create', [CourseController::class, 'createCourse']);
     Route::get('/admin/show/{id}',[UserController::class, 'show']);
     Route::get('/user/show/{id}',[UserController::class, 'show']);
-    Route::get('/user/disable/{id}', [UserController::class, 'disable_user']);
-    Route::post('/course/create', [CourseController::class, 'createCourse']);
-    Route::get('/course/get_all', [CourseController::class, 'getAllCourse']);
-    Route::apiResource('categories', \Category\CategoryController::class);
-    Route::post('/course/create', [CourseController::class, 'createCourse']);
     Route::post('/course/create/confirm', [CourseController::class , 'createConfirm']);
-    Route::get('/course/edit/{id}', [CourseController::class, 'editCourse']);
     Route::put('/course/update/{id}', [CourseController::class, 'updateCourse']);
     Route::put('/course/update/confirm/{id}', [CourseController::class, 'updateConfirm']);
     Route::delete('/course/delete/{id}', [CourseController::class, 'deleteCourse']);
-    Route::get('/course/search/{keyword}', [CourseController::class, 'searchCourse']);
+    Route::get('/user/list',[UserController::class, 'index']);
+    Route::get('/user/disable/{id}', [UserController::class, 'disable_user']);
+    Route::apiResource('categories', \Category\CategoryController::class);
 });
 
